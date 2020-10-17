7874ddaface8c700324d00b608754d5fb0484f47
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-18 01:36:52 +0200

    generate pdf for orders

--- myshop/settings.py
+++ myshop/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
--- orders/admin.py
+++ orders/admin.py
@@ -48,6 +48,14 @@ def order_detail(obj: Order):
     return mark_safe(f"<a href='{url}'>View</a>")
 
 
+def order_pdf(obj):
+    url = reverse("orders:admin_order_pdf", args=[obj.id])
+    return mark_safe(f"<a href='{url}'>PDF</a>")
+
+
+order_pdf.short_description = "Invoice"
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -62,6 +70,7 @@ class OrderAdmin(admin.ModelAdmin):
         "created",
         "updated",
         order_detail,
+        order_pdf,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
