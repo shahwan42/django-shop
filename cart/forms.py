7529bec4e62282b673013e7df2316a4d2c535626
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-17 16:00:45 +0200

    fix cart update

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
