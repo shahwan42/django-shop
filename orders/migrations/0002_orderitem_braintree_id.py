d84db8e7fe6d0932e5e7465b392df41c8fac198e
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-24 18:42:32 +0200

    coupon app, mode, admin + black formatting

--- orders/migrations/0002_orderitem_braintree_id.py
+++ orders/migrations/0002_orderitem_braintree_id.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orders', '0001_initial'),
+        ("orders", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='orderitem',
-            name='braintree_id',
+            model_name="orderitem",
+            name="braintree_id",
             field=models.CharField(blank=True, max_length=150),
         ),
     ]
