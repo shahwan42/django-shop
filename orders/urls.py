222135adda1f9cbad9f71df4f849e7128f412457
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-31 11:26:50 +0200

    translate url patterns

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
+
 from . import views
 
 app_name = "orders"
 
 urlpatterns = [
-    path("create/", views.order_create, name="order_create"),
+    path(_("create/"), views.order_create, name="order_create"),
     path(
         "admin/order/<int:order_id>/",
         views.admin_order_detail,
--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
+
 from . import views
 
 app_name = "payment"
 
 urlpatterns = [
-    path("process/", views.payment_process, name="process"),
-    path("done/", views.payment_done, name="done"),
-    path("canceled/", views.payment_canceled, name="canceled"),
+    path(_("process/"), views.payment_process, name="process"),
+    path(_("done/"), views.payment_done, name="done"),
+    path(_("canceled/"), views.payment_canceled, name="canceled"),
 ]
